9468fd56018fbbded6247eb265b3a63b53bb3da5
AmareshJakka <amareswararaojakka@gmail.com>
2021-01-23 12:10:39 +0530

    "deleted unwanted code"

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable} from 'rxjs';
-import { combineAll, map } from 'rxjs/operators';
+import {  map } from 'rxjs/operators';
 import{empModal} from '../modals/emp.modal'
 import{IEmployee} from '../modals/employee';
 
@@ -17,22 +17,27 @@ export class EmployeeService {
 
   // HttpClient API post() method => Create employee
   createEmployee(employee): Observable<empModal> {
-    return this.http.post<empModal>(`${this.localUrl}`, JSON.stringify(employee))
+    return this.http.post<empModal>(`${this.baseUrl}`, JSON.stringify(employee))
     
   }  
-  // HttpClient API get() method => to get data
-  getEmployeesList():Observable<empModal> {
+  // HttpClient API get() method => to get data from data.json
+  getlocalList():Observable<empModal> {
      return this.http.get<empModal>(`${this.localUrl}`)
-     //pipe(map(res=>{
-    //   console.log(res);
-    //  const emp= JSON.parse(JSON.stringify(res));
-    //   console.log(emp.data);
-    //   localStorage.setItem("employees",JSON.stringify(res));
-    //  return emp.data;
-    // }))
   }
 
   getById(id): Observable<empModal> {
     return this.http.get<empModal>(`${this.baseUrl}/${id}`)
   }
+
+  // HttpClient API get() method => to get data from backend
+  getEmployeesList():Observable<empModal> {
+    return this.http.get<empModal>(`${this.baseUrl}`).pipe(map(res=>{
+     console.log(res);
+    const emp= JSON.parse(JSON.stringify(res));
+     console.log(emp.data);
+     localStorage.setItem("employees",JSON.stringify(res));
+    return emp.data;
+   }))
+ }
+
 }
